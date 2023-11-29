a8a6acee60898f03f6a02541b3e5fa47ae4a484b
Andrzej Darowski <andrzej.dar@gmail.com>
2023-11-29 15:03:36 +0100

    cors policy for localhost

--- Renovation.API/Program.cs
+++ Renovation.API/Program.cs
@@ -84,6 +84,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
     });
 
+builder.Services.AddCors((options) => {
+    options.AddPolicy("AllowLocalhost3000", builder => builder.WithOrigins("https://localhost:3000").AllowAnyHeader().AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
